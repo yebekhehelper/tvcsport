ae96af2d6e0876c3a5dc134eb9c9b7027a25ecf7
YeBeKhe-Helper <129661618+yebekhehelper@users.noreply.github.com>
2024-02-06 21:08:28 +0330

    Update api.php

--- api.php
+++ api.php
@@ -1,2 +1,7 @@
 <?php
-file_put_contents("todayMatches.json", file_get_contents("https://web-api.varzesh3.com/v1.0/livescore/today")) ;
+$todayMatches = json_decode(file_get_contents("https://web-api.varzesh3.com/v1.0/livescore/today"), true);
+$todayMatchesOutput = [
+  "timeStamp" => time(),
+  "todayMatches" => $todayMatches
+];
+file_put_contents("todayMatches.json", json_encode($todayMatchesOutput, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE)) ;
