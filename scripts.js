408d21cd60173e36bbe22dadc8540f5d4c14fb10
YeBeKhe-Helper <129661618+yebekhehelper@users.noreply.github.com>
2024-02-07 00:15:44 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -179,9 +179,10 @@ fetch('todayMatches.json')
 				const row = document.createElement('tr');
 				// Check if the matchId is in any of the matchIds arrays
 				leagues.forEach(league => {
-					const matchIds = league.dates[0].matchIds;
+					const matchIds0 = league.dates[0].matchIds;
+					const matchIds1 = league.dates[1].matchIds || [];
 					const matchId = match.sportId;
-					if (matchIds.includes(matchId)) {
+					if (matchIds0.includes(matchId) || matchIds1.includes(matchId)) {
 						// Create a new image element
 						var img = document.createElement('img');
 						img.src = league.logo; // Set the source of the image
